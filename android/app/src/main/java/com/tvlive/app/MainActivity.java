fe627400b7b489a36ff596e534d046bc64c44fbe
Yifan <yifan@tvlive.app>
2026-01-30 03:17:10 +0000

    Add: Missing Java source files

--- android/app/src/main/java/com/tvlive/app/MainActivity.java
+++ android/app/src/main/java/com/tvlive/app/MainActivity.java
@@ -1,21 +1,16 @@
 package com.tvlive.app;
 
+import android.os.Bundle;
+import android.view.KeyEvent;
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
-import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
+/**
+ * Main Activity for VideoViewer app
+ */
 public class MainActivity extends ReactActivity {
+
     @Override
     protected String getMainComponentName() {
         return "tv-live-app";
-    }
-
-    @Override
-    protected ReactActivityDelegate createReactActivityDelegate() {
-        return new DefaultReactActivityDelegate(
-            this,
-            getMainComponentName(),
-            DefaultNewArchitectureEntryPoint.getEnabled());
     }
 }
--- android/app/src/main/java/com/tvlive/app/MainApplication.java
+++ android/app/src/main/java/com/tvlive/app/MainApplication.java
@@ -1,58 +1,23 @@
 package com.tvlive.app;
 
-import android.app.Application;
-import com.facebook.react.PackageList;
-import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactNativeHost;
-import com.facebook.react.ReactPackage;
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
-import com.facebook.react.defaults.DefaultReactNativeHost;
-import com.facebook.soloader.SoLoader;
+import android.os.Bundle;
+import android.view.View;
 
-import java.util.List;
-
-public class MainApplication extends Application implements ReactApplication {
-    private final ReactNativeHost mReactNativeHost = new DefaultReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-            return BuildConfig.DEBUG;
-        }
-
-        @Override
-        protected List<ReactPackage> getPackages() {
-            @SuppressWarnings("UnnecessaryLocalVariable")
-            List<ReactPackage> packages = new PackageList(this).getPackages();
-            return packages;
-        }
-
-        @Override
-        protected String getJSMainModuleName() {
-            return "index";
-        }
-
-        @Override
-        protected boolean isNewArchEnabled() {
-            return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-        }
-
-        @Override
-        protected Boolean isHermesEnabled() {
-            return BuildConfig.IS_HERMES_ENABLED;
-        }
-    };
+/**
+ * Main Application class for VideoViewer app
+ */
+public class MainApplication extends ReactApplication {
 
     @Override
-    public ReactNativeHost getReactNativeHost() {
-        return mReactNativeHost;
+    protected String getMainComponentName() {
+        return "tv-live-app";
     }
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-        SoLoader.init(this, false);
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            DefaultNewArchitectureEntryPoint.load();
-        }
-        ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    protected boolean isDebug() {
+        return BuildConfig.DEBUG;
     }
 }
